2fea46f2821d76b73630392093172968dc9b7856
BoykaDan <danlang@mail.ustc.edu.cn>
2017-07-12 15:46:29 +0800

    founctipn 注册

---
+++ app/Task.php
@@ -0,0 +1,26 @@
+<?php
+/*@author:danblang
+*@time:2017.7.12
+ * model_name:task
+ * */
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /*
+     * This attributs that are mass assignable.
+     * @var array
+     * */
+    protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+}
+
+
